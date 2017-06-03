b0580325d188d49dce922c609e6020881253095e
champillon <passapong.t@gmail.com>
2017-06-03 16:29:53 +0700

    removed get, post, postReturn

--- src/main/scala/http/HttpAction.scala
+++ src/main/scala/http/HttpAction.scala
@@ -15,30 +15,7 @@ object HttpAction {
       .header("TOKEN", token)
       .postData(json.toText)
       .asString
-      .code == 200
-
-  def post(url: String, token: String): Boolean =
-    Http(s"${url}")
-      .header("Content-Type", "application/json")
-      .header("TOKEN", token)
-      .postData("{}")
-      .asString
-      .code == 200
-
-  def postReturn[T: Manifest](url: String, json: Json): Option[T] =
-    Json.toObject[T](
-      Http(s"${url}")
-        .header("Content-Type", "application/json")
-        .postData(json.toText)
-        .asString
-        .body)
-
-  def get[T: Manifest](url: String): Option[T] =
-    Json.toObject[T](
-      Http(s"${url}")
-        .header("Content-Type", "application/json")
-        .asString
-        .body)
+      .code == 201
 
   def get[T: Manifest](url: String, token: String): Option[T] =
     Json.toObject[T](
